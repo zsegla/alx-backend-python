7e6cdbc4ca840b37323c2fe6016171d0aa48131c
Fekreselassie <fekruson@gmail.com>
2025-07-27 17:41:52 +0300

    removing the messaging app

--- messaging_app/chats/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChatsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chats'
-    verbose_name = 'Chat Application'
